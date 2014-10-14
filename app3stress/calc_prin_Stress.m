53b0f8f60782430d9f68640bb94c44d98dda01ba
yejiyang <yejiyang@gmail.com>
2014-10-14 17:49:57 -0500

    update

--- app3stress/calc_prin_Stress.m
+++ app3stress/calc_prin_Stress.m
@@ -1,6 +1,7 @@
-%%
+%% about this script
 % Author: Jiyang Ye (yejiyang@gmail.com)
 % initial date: Jul 30, 2014
+% revised Oct 14, 2014
 % this program aim to calculate 2-d principal stress, same with strain, 
 % input:  the 2-d stress, sij = [s11 s12; s21 s22], s12 = s21; 
 % output: the principal stress, pij = [max_sij 0; 0 min_sij], 
@@ -12,36 +13,51 @@
 clear; clc;
 
 %% read the initial 2-d stress state data
-init_str = load('./input/input_stress_for_prin_stress');
-%init_str = xlsread('./input/input_stress_for_prin_stress.csv');
+ori_str = load('./input/input_stress_for_prin_stress');
+%ori_str = xlsread('./input/input_stress_for_prin_stress.csv');
 
-%% call the function [pij, sitaP_max] = principal_Stress_trans(sij) to calculate 
-% the principal stress
-for i=1:length(init_str)
+%% call the function [output] = principal_2d(sij) to calculate 
+% the principal stress;
+size_ori = size(ori_str);
+% the output size of function principal_2d equals 4;
+size_out = 4;
+%pij =zeros(size_ori(1),size_ori(2)+size_out);
+
+pij =zeros(size_ori(1),size_out);
+%pij=zeros(10,4);
+
+for i=1:length(ori_str)
 %for i=1:10
 	sij = zeros(2);
-	sij(1,1) = init_str(i,4);
-	sij(2,2) = init_str(i,5);
-	sij(1,2) = init_str(i,6);
-	sij(2,1) = init_str(i,6);
+	sij(1,1) = ori_str(i,4);
+	sij(2,2) = ori_str(i,5);
+	sij(1,2) = ori_str(i,6);
+	sij(2,1) = ori_str(i,6);
 	%sij
 	
 	% call the function
-	[pij , sitaP_max] = principal_Stress_trans(sij);
+	[output] = principal_2d(sij);
 	
-	% storage the data
-	pij_final(i,1) = init_str(i,1); % layer number	
-	pij_final(i,2) = init_str(i,2); % x or longi
-	pij_final(i,3) = init_str(i,3); % y or lat
-	pij_final(i,4) = pij(1,1);      % max_principal_stress
-	pij_final(i,5) = pij(2,2);      % min_principal_stress
-	pij_final(i,6) = sitaP_max;     % countclockwise angle from x to max_principal_stress	
+	% get the original data
+	%pij(i,1:size_ori(2)) = ori_str(i,1:size_ori(2)); % layer number	
+    % get the principal data
+	%pij(i, (size_ori(2)+1):(size_ori(2)+size_out)) = output;
+    pij(i, 1:size_out) = output;
 	
 end
 
-%pij_final
 
-% output the results
-csvwrite('principal_stress.data', pij_final); % output the results
+%% output the results
 
-%end
+%csvwrite('principal_stress.data', pij); % output the results
+
+%save prin_stress.out pij -ASCII
+
+fileID = fopen('prin_stress.txt','w');
+%fprintf(fileID,'%6s %12s\n','x','exp(x)');
+% fprintf write in column order!!!, so pij ->pij'
+formatSpec='%4.2f   %4.2f   %3.2f   %4.2f\n';
+fprintf(fileID,formatSpec,pij');
+fclose(fileID);
+
+%% end
