5dee66a9c94d6702172bd33268ae35dac4c4dcf7
yejiyang <yejiyang@gmail.com>
2014-10-18 15:26:56 -0500

    update

---
+++ vep-beachball/hui/app21/scompute_f90/src/scompute_f90.f90
@@ -0,0 +1,12 @@
+! ============================================================================
+! Name        : scompute_f90.f90
+! Author      : Jiyang Ye
+! Version     :
+! Copyright   : Your copyright notice
+! Description : Hello World in Fortran
+! ============================================================================
+
+program scompute_f90
+    implicit none
+    print *, "Hello MPI World"
+end program
