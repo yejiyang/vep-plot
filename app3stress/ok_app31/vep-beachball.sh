3312db737b36411eda4449bc8bdeb86a99143f5b
yejiyang <yejiyang@gmail.com>
2014-10-21 13:30:28 -0500

    app3stress-ok

--- app3stress/ok_app31/vep-beachball.sh
+++ app3stress/ok_app31/vep-beachball.sh
@@ -4,11 +4,13 @@
 # this file includes the data to plot beachball, calculated from Yang's method.
 # the original file is from '/home/jiyang/opensource/vep-plot/app3stress/ori_results/app32', calculated by 'p12345.f'
 
-input=./input/sagrid.32.txt
+input=./input/sagrid.31.txt
+inputybb=./input/beachball.31.txt
+inputprinstr=./input/prin_stress.txt
 #input=./ores.$1/sagrid.$1.txt
 #prepare the data
 #title=$1.beachball.vep
-title=32.beachball.vep
+title=31.beachball.vep
 psfile=${title}.ps
 
 range="-R75/225/100/300"
@@ -33,12 +35,17 @@ awk '{if(($1==1)&& int(($2+5000)*10/100000)==(($2+5000)*10/100000) && int($3*10/
 # beachball from hui
 #psmeca bb.txt  -R -J -Sx0.09i -Ggrey -Ewhite  -O  -W2  -C -V -K>>$psfile
 
-awk '{if(int(($1+5000)*10/100000)==(($1+5000)*10/100000) && int($2*10/100000)==($2*10/100000))  print $1/1000,$2/1000,$3,$4,$5,$6,$7,$8,$9,$10,$11/1000,$12/1000}' beachball.txt > yang.txt
+awk '{if(int(($1+5000)*10/100000)==(($1+5000)*10/100000) && int($2*10/100000)==($2*10/100000))  print $1/1000,$2/1000,$3,$4,$5,$6,$7,$8,$9,$10,$11/1000,$12/1000}' $inputybb > yang.txt
 
 # beachball from yang
 psmeca yang.txt -R  -J  -C -O -V  -Sd0.25i/1 -Gred -Ewhite -K>> $psfile
 
-awk '{if(int(($2+5)*10/100)==(($2+5)*10/100) && int($3*10/100)==($3*10/100))  print $2,$3,$7,$8,-$9}' prin_stress1.txt > prin.txt
+# about the direction of 2D principal stress
+# our matlab output, extension--positive, compression--negative, but the sita equals to I1 counter-clockwise to x-axis.
+# in gmt, psvelo, -Sx option, extension and compression definition the same. But the angle defition is different. 
+# the angle in gmt, is azimuth of II2 in degrees CW (clockwise) from North.
+# So we need transform the matlab output sita to GMT angle. I1=sita, I2=sita+90, angle=90-sita=90-sita-90=-sita, this's why -$9
+awk '{if(int(($2+5)*10/100)==(($2+5)*10/100) && int($3*10/100)==($3*10/100))  print $2,$3,$7,$8,-$9}' $inputprinstr > prin.txt
 # -A default -A0.03/0.12/0.09
 psvelo prin.txt -A0.01/0.6/0.1 -W0.25p,blue  -R  -J   -O -V  -Sx0.06  -K>> $psfile
 
